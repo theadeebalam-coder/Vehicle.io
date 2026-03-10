ade4fd5b17b33b902735597adf6c5c40a2683fe0
Bharat.io <theadeebalam@gmail.com>
2026-03-10 17:41:38 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,43 @@
 const CACHE_NAME = "canacot-cache-v1";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css"
+
+"./",
+"./index.html",
+"./style.css",
+"./manifest.json",
+"./logo.svg"
+
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache => {
+
+return cache.addAll(urlsToCache);
+
+})
+
+);
+
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response => {
+
+return response || fetch(event.request);
+
+})
+
+);
+
 });
